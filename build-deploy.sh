953993fe0c21c645b98b0b0aa782f5825be9d79b
wikislate <wikislate@gmail.com>
2019-03-17 17:30:28 +0530

    sudo: list

--- build-deploy.sh
+++ build-deploy.sh
@@ -7,5 +7,6 @@ sudo cp -rf $MAVEN_PROJECT/target/*.war $TOMCAT_DIR
 sudo chmod -R 775 $TOMCAT_DIR
 ps -ef | grep tomcat
 sudo cd $TOMCAT_DIR/../bin
+ls -l
 ./shutdown.sh && ./startup.sh
 ps -ef | grep tomcat
