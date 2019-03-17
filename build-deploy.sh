65ed26ad4b07b86ea864bf05a48e28dc3bec1f06
wikislate <wikislate@gmail.com>
2019-03-17 17:41:16 +0530

    bash

--- build-deploy.sh
+++ build-deploy.sh
@@ -6,5 +6,5 @@ mvn clean install
 sudo cp -rf $MAVEN_PROJECT/target/*.war $TOMCAT_DIR
 sudo chmod -R 775 $TOMCAT_DIR
 ps -ef | grep tomcat
-sudo .$TOMCAT_DIR/../bin/shutdown.sh && sudo .$TOMCAT_DIR/../bin/startup.sh
+sudo bash $TOMCAT_DIR/../bin/shutdown.sh && sudo bash $TOMCAT_DIR/../bin/startup.sh
 ps -ef | grep tomcat
